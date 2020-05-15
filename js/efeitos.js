97ef97254d91cc9c9e0df24964a743234fea0c48
Eduardo Answer <eduardonzinga111@hotmail.com>
2020-05-15 16:11:57 +0100

    Changing smartphone config and carousel fixed

--- js/efeitos.js
+++ js/efeitos.js
@@ -22,14 +22,20 @@ $(document).ready(function () {
     $(".thumbnails").owlCarousel({
         loop: true,
         margin: 20,
-        nav: true,
+        /*autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause:true,*/        
+        autoplay: 6000,
+        slideSpeed: 10000,
+        paginationSpeed: 400,
+        /*nav: true,*/
         navText: ["Anterior", "Próximo"],
         responsive: {
             0: {
                 items: 1
             },
             480: {
-                items: 3
+                items: 1
             },
             1000: {
                 items: 4
@@ -37,5 +43,20 @@ $(document).ready(function () {
 
         }
     });
+
+    /*For smartphone */
+    var owl = $('.thumbnails');
+    owl.owlCarousel();
+    // Go to the next item
+    $('#btn-news-next').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('#btn-news-prev').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+
 
 });
